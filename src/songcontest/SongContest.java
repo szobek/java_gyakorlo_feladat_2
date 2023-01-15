0e2af9cf05cf2425b14639138281fdd37f3620a4
Kunszt Norbert <kunszt.norbert@gmal.com>
2023-01-15 20:14:09 +0100

    songcontest game search winner and write

--- src/songcontest/SongContest.java
+++ src/songcontest/SongContest.java
@@ -4,13 +4,28 @@ package songcontest;
 import java.util.Random;
 
 public class SongContest {
-    public void start() {
         Registration registration = new Registration();
+    public void start() {
         registration.writeAuthors();
         for (int i = 0; i < registration.authors.length; i++) {
             int point = new Random().nextInt(50);
             registration.authors[i].setPoint(point);
         }
+        System.out.println("----------------------------------------------------------------");
         registration.writeAuthors();
+        System.out.println("----------------------------------------------------------------");
+        Author winner = searchWinner();
+        System.out.println("a nyertes: "+winner.getName()+" (" +winner.getPoint()+")");
+    }
+    public Author searchWinner(){
+        Author winner=null;
+        int max=0;
+        for (Author author: registration.authors){
+            if(author.getPoint()>max){
+                max=author.getPoint();
+                winner=author;
+            }
+        }
+        return winner;
     }
 }
